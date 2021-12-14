acaf624be3e0250e68b1b2b8bf0ac1a2625b9073
simon.islam <simon.islam@brilliant.com.bd>
2021-12-14 10:28:48 +0600

    added vue cli compression

---
+++ vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    pluginOptions: {
+      compression:{
+        brotli: {
+          filename: '[file].br[query]',
+          algorithm: 'brotliCompress',
+          include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+          compressionOptions: {
+            params: {
+              [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+            },
+          },
+          minRatio: 0.8,
+        },
+        gzip: {
+          filename: '[file].gz[query]',
+          algorithm: 'gzip',
+          include: /\.(js|css|html|svg|json|png|gif)(\?.*)?$/i,
+          minRatio: 0.8,
+        }
+      }
+    }
+  }
