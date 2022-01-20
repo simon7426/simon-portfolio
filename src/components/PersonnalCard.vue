b8a6e816845ff906a719f883b74ac3d9a789d3c9
simon.islam <simon.islam@brilliant.com.bd>
2022-01-20 22:56:57 +0600

    added resume to website

--- src/components/PersonnalCard.vue
+++ src/components/PersonnalCard.vue
@@ -6,6 +6,7 @@
     <div class="data"><strong>City:</strong> {{ contact.city }}</div>
     <div class="data"><strong>Languages:</strong> {{ contact.languages }}</div>
     <SocialBar :links="links"/>
+    <div class="button" v-on:click="resumeClick">Resume</div>
   </div>
 </template>
 
@@ -17,6 +18,11 @@ export default {
     props: ['contact', 'links'],
     components: {
         SocialBar,
+    },
+    methods: {
+      resumeClick() {
+        window.open(this.links.resume, "_blank");
+      }
     }
 }
 </script>
@@ -25,6 +31,30 @@ export default {
     @import '@/styles/constants.scss';
 
     .data {
-        margin-bottom: 15px;
+      margin-bottom: 15px;
+    }
+    .button {
+      width: 15rem;
+      background-color: #3c6e71;
+      border: none;
+      color: white;
+      padding: 1rem 1rem;
+      text-align: center;
+      text-decoration: none;
+      display: inline-block;
+      border-radius: 1rem;
+      font-size: 1.25rem;
+      transition-duration: 0.4s;
+    }
+    .button:hover {
+      background-color: map-get($colors, light); /* Green */
+      color: black;
+      cursor: pointer;
+      box-shadow: 0 1rem 1rem 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+    }
+    .button:active {
+      background-color: map-get($colors, light);
+      box-shadow: 0 .5rem #666;
+      transform: translateY(.25rem);
     }
 </style>
