0e355aae394943ad2a73a50c6604159317060e9f
simon.islam <simon.islam@brilliant.com.bd>
2022-01-20 23:24:29 +0600

    accesibility changes for lighthouse

--- src/components/SocialBar.vue
+++ src/components/SocialBar.vue
@@ -2,32 +2,32 @@
   <div class="social-wrap">
     <ul>
       <li>
-        <a :href="links.facebook" target="_blank">
+        <a :href="links.facebook" target="_blank" alt="facebook" rel="noopener noreferrer">
           <font-awesome-icon class="icon" :icon="['fab', 'facebook-square']" />
         </a>
       </li>
       <li>
-        <a :href="links.twitter" target="_blank">
+        <a :href="links.twitter" target="_blank" alt="twitter" rel="noopener noreferrer">
           <font-awesome-icon class="icon" :icon="['fab', 'twitter-square']" />
         </a>
       </li>
       <li>
-        <a :href="links.linkedin" target="_blank">
+        <a :href="links.linkedin" target="_blank" alt="linkedin" rel="noopener noreferrer">
           <font-awesome-icon class="icon" :icon="['fab', 'linkedin']" />
         </a>
       </li>
       <li>
-        <a :href="links.reddit" target="_blank">
+        <a :href="links.reddit" target="_blank" alt="reddit" rel="noopener noreferrer">
           <font-awesome-icon class="icon" :icon="['fab', 'reddit-square']" />
         </a>
       </li>
       <li>
-        <a :href="links.github" target="_blank">
+        <a :href="links.github" target="_blank" alt="github" rel="noopener noreferrer">
           <font-awesome-icon class="icon" :icon="['fab', 'github-square']" />
         </a>
       </li>
       <li>
-        <a :href="links.github" target="_blank">
+        <a :href="links.github" target="_blank" alt="gitlab" rel="noopener noreferrer">
           <font-awesome-icon class="icon" :icon="['fab', 'gitlab']" />
         </a>
       </li>
