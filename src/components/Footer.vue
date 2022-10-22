6b76b429aa10cf03d63c8e6f379fb88b5aba9e81
simon.islam <simon.islam@brilliant.com.bd>
2022-10-22 20:51:12 +0600

    updated package.json and removed vue-axios. Build working

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer">
     <div>
         <font-awesome-icon class="copyright-icon" icon="copyright"/> 
-        2021 {{ name }} 
+        2022 {{ name }} 
     </div>
     <SocialBar :links="links"/>
   </footer>
